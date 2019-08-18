fd0811deacd7bb386ed1f22d5cefe58dca9d608d
abhishek <abhishek.abhi57838@gmail.com>
2019-08-18 22:34:17 +0200

    adding data script

---
+++ Prepare_data.sh
@@ -0,0 +1,17 @@
+mkdir Shapes
+cd Shapes
+wget 'https://drive.google.com/open?id=1eMN1pVXFbxbPQtpHEUmR5QcE639sTLEE'
+unzip FAUST_original.zip && rm FAUST_original.zip
+#cd ..
+
+#cd Shapes
+wget 'https://drive.google.com/open?id=1R1cpd1npjzwuO09UBWtV3jkdfI_agX3l'
+unzip FAUST_remesh.zip && rm FAUST_remesh.zip
+#cd ..
+
+#cd Shapes
+wget 'https://drive.google.com/open?id=16pIe2U-hZdn7FUMSTEW0yeMpbev8oJ24'
+unzip Scape_remesh.zip && rm Scape_remesh.zip
+#cd ..
+
+
