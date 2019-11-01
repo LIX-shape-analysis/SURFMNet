25513aada682bf1db1170427efcfa47ce6e1b639
Abhishek Sharma <abhishek.abhi57838@gmail.com>
2019-11-01 14:59:14 +0100

    Update Prepare_data.sh
    included off files and updated the gdrive link

--- Prepare_data.sh
+++ Prepare_data.sh
@@ -1,6 +1,7 @@
 mkdir Shapes
 #cd Shapes
-./gdown.pl 'https://drive.google.com/open?id=1eMN1pVXFbxbPQtpHEUmR5QcE639sTLEE' Shapes/FAUST_original.zip
+./gdown.pl 'https://drive.google.com/open?id=1o7s2-Y8mMcFm5Vu9RaXYctiN3GjmboUy' Shapes/FAUST_original.zip
+
 unzip Shapes/FAUST_original.zip && rm Shapes/FAUST_original.zip
 #cd ..
 
