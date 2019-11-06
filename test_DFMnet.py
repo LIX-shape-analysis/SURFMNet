6998aa55c0d96804f2282e5cf35bd93e823a6303
abhishek <abhishek.abhi57838@gmail.com>
2019-11-06 13:31:23 +0100

    modified for testing

--- test_DFMnet.py
+++ test_DFMnet.py
@@ -7,16 +7,12 @@ from scipy.spatial import cKDTree
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
-flags.DEFINE_integer('num_evecs', 120,
-                     'number of eigenvectors used for representation')
-flags.DEFINE_integer('num_model', 5000, '')
-flags.DEFINE_string('test_shapes_dir', './Shapes/', '')
-flags.DEFINE_string('files_name', 'tr_reg_', 'name common to all the shapes')
-flags.DEFINE_string('log_dir', './Testing/',
-                    'directory to save targets results')
-flags.DEFINE_string('matches_dir', './Matches/',
-                    'directory to matches')
-
+flags.DEFINE_integer('num_evecs', 120, 'number of eigenvectors used for representation')
+flags.DEFINE_integer('num_model', 10000, '')
+flags.DEFINE_string('test_shapes_dir', '../UnsupervisedFMapNet/Scr/Unsupervised_FMnet/Shapes/SCAPE_r/MAT_SHOT/', '')
+flags.DEFINE_string('files_name', 'mesh', 'name common to all the shapes')
+flags.DEFINE_string('log_dir', './Training/SCAPE_r/1500/', 'directory to save targets results')
+flags.DEFINE_string('matches_dir', './Matches/SCAPE_r/1500/', 'directory to matches')
 
 def get_test_pair_source(source_fname):
     input_data = {}
@@ -32,7 +28,6 @@ def get_test_pair_source(source_fname):
     del input_data['target_shot']
     input_data['source_evals'] = np.transpose(input_data['target_evals'])
     del input_data['target_evals']
-
     return input_data
 
 
@@ -42,7 +37,6 @@ def get_test_pair_target(target_fname):
 
     input_data.update(sio.loadmat(target_file))
     input_data['target_evals'] = np.transpose(input_data['target_evals'])
-
     return input_data
 
 
@@ -53,11 +47,9 @@ def run_test():
     sess = tf.Session(config=config)
 
     print('restoring graph...')
-    saver = tf.train.import_meta_graph('%smodel.ckpt-%s.meta'
-                                       % (FLAGS.log_dir, FLAGS.num_model))
+    saver = tf.train.import_meta_graph('%smodel.ckpt-%s.meta' % (FLAGS.log_dir, FLAGS.num_model))
     saver.restore(sess, tf.train.latest_checkpoint('%s' % FLAGS.log_dir))
     graph = tf.get_default_graph()
-
     # Retrieve placeholder variables
     source_evecs = graph.get_tensor_by_name('source_evecs:0')
     source_evecs_trans = graph.get_tensor_by_name('source_evecs_trans:0')
@@ -73,44 +65,25 @@ def run_test():
                     'matrix_solve_ls/cholesky_solve/MatrixTriangularSolve_1:0'
                                       )
 
-    for i in range(80, 99):
+    for i in range(60, 69):
         input_data_source = get_test_pair_source(FLAGS.files_name + '%.3d' % i)
         source_evecs_ = input_data_source['source_evecs'][:, 0:FLAGS.num_evecs]
 
-        for j in range(i+1, 100):
+        for j in range(i+1, 70):
             t = time.time()
 
-            input_data_target = get_test_pair_target(FLAGS.files_name +
-                                                     '%.3d' % j)
+            input_data_target = get_test_pair_target(FLAGS.files_name +'%.3d' % j)
 
             feed_dict = {
                 phase: True,
                 source_shot: [input_data_source['source_shot']],
                 target_shot: [input_data_target['target_shot']],
-                source_evecs: [input_data_source['source_evecs'][
-                                                        :,
-                                                        0:FLAGS.num_evecs
-                                                                 ]
-                               ],
-                source_evecs_trans: [input_data_source[
-                                    'source_evecs_trans'
-                                                      ][
-                                                        0:FLAGS.num_evecs,
-                                                        :]
-                                         ],
-                source_evals: [input_data_source[
-                                                'source_evals'
-                                                     ][0][0:FLAGS.num_evecs]],
-                target_evecs: [input_data_target[
-                                                'target_evecs'
-                                                     ][:, 0:FLAGS.num_evecs]],
-                target_evecs_trans: [input_data_target[
-                                       'target_evecs_trans'][
-                                                             0:FLAGS.num_evecs,
-                                                             :]
-                                         ],
-                target_evals: [input_data_target[
-                                       'target_evals'][0][0:FLAGS.num_evecs]]
+                source_evecs: [input_data_source['source_evecs'][:,0:FLAGS.num_evecs]],
+                source_evecs_trans: [input_data_source['source_evecs_trans'][0:FLAGS.num_evecs,:]],
+                source_evals: [input_data_source['source_evals'][0][0:FLAGS.num_evecs]],
+                target_evecs: [input_data_target['target_evecs'][:, 0:FLAGS.num_evecs]],
+                target_evecs_trans: [input_data_target['target_evecs_trans'][0:FLAGS.num_evecs,:]],
+                target_evals: [input_data_target['target_evals'][0][0:FLAGS.num_evecs]]
                              }
 
             Ct_est_ = sess.run([Ct_est], feed_dict=feed_dict)
@@ -122,8 +95,7 @@ def run_test():
             dist, indices = kdt.query(target_evecs_, n_jobs=-1)
             indices = indices + 1
 
-            print("Computed correspondences for pair: %s, %s." % (i, j) +
-                  " Took %f seconds" % (time.time() - t))
+            print("Computed correspondences for pair: %s, %s." % (i, j) +" Took %f seconds" % (time.time() - t))
 
             params_to_save = {}
             params_to_save['matches'] = indices
